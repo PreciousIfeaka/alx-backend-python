44c5e185ff5a8f8ba554a039964da15e01bfa712
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-10-13 08:14:21 +0000

    concatenated strings

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 """This module uses a type annotated function that adds its arguments
 """
 
+
 def add(a: float, b: float) -> float:
     """adds two arguments"""
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that concatenates two strings
+   passed as arguments
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
