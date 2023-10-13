60e8749d6af9f03de110c51978a9ee460782d987
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-10-13 08:08:20 +0000

    added two typed arguments

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module uses a type annotated function that adds its arguments
+"""
+
+def add(a: float, b: float) -> float:
+    """adds two arguments"""
+    return a + b
