e07b544ea0acc98e1b85c7e45271bdc9f1ce4f0d
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-10-13 11:45:02 +0000

    returned a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""This module contains a function that takes a string and an int or float
+   as arguments and returns a tuple.
+"""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''this function returns the square of the value argument'''
+
+    return (k, v**2)
