4fd9e1fa3e16f1967ad6315523449f3dfcdfaa19
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-10-13 14:46:26 +0000

    type-annotated a given function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''returns a sequential tuple'''
     return [(i, len(i)) for i in lst]
