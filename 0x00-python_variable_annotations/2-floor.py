851ab714fee227089a3344656b1acf4f7944f2f4
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-10-13 08:22:54 +0000

    floored a float and documented task 1

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a function that takes the floor
+   of a given float number
+"""
+
+import math
+
+def floor(n: float) -> int:
+    '''gives the floor of n'''
+    return math.floor(n)
