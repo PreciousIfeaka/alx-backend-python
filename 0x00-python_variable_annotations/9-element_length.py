2ead0765b80fa5ea676516d53f50020be3253009
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-10-13 14:43:30 +0000

    type-annotated a given function

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module annotates the below function parameters and returns values with
+   the appropriate types
+"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
